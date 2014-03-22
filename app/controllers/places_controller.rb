13ba438155f8bfcf4a4eba1d3c072d88729c187c
Promoveo <zappeln@gmail.com>
2014-03-22 18:39:07 -0500

    refactor code

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def index
     place_id = Place.find_by_name(params[:name]).id
-    @traffic = Traffic.where(place_id: place_id) 
+    @chart = Traffic.traffic_report(place_id) 
   end
 end
--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -1,4 +1,15 @@
 class Traffic < ActiveRecord::Base
   attr_accessible :wait, :place_id
   belongs_to :place
+
+  def self.traffic_report(store_id)
+    location_report = Traffic.where(place_id: store_id) 
+    chart = {}
+    location_report.each do |t|
+      if t.created_at.strftime('%A') == Time.now.strftime('%A')
+        chart.merge!({t.created_at => t.wait})
+      end
+    end
+    chart
+  end
 end
