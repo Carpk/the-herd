175957184019337393dbee6370004cea0bae4f5d
Promoveo <zappeln@gmail.com>
2014-03-22 15:09:35 -0500

    working graph

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,6 @@
 class PlacesController < ApplicationController
   def index
-    puts "NAME: #{params[:name]}"
     place_id = Place.find_by_name(params[:name]).id
-    puts "PLACE ID: #{place_id}"
     @traffic = Traffic.where(place_id: place_id) 
   end
 end
