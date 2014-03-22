80a271204795fdbbee61239b7fa72f6b0d9336e7
Promoveo <zappeln@gmail.com>
2014-03-22 14:04:11 -0500

    passing data to graph

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,8 @@
 class PlacesController < ApplicationController
   def index
-    
+    puts "NAME: #{params[:name]}"
+    place_id = Place.find_by_name(params[:name]).id
+    puts "PLACE ID: #{place_id}"
+    @traffic = Traffic.where(place_id: place_id) 
   end
 end
