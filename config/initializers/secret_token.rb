9c7e264155941f77406858cb0efe3b911bf71ca3
Promoveo <zappeln@gmail.com>
2014-03-22 00:16:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herd::Application.config.secret_token = '7ffbe905be2e8e978173d4993b987e8cd3c7adac5514412563f09958576a0873cd7b0e9803a2cf1e3527877b909486381b15078f64556120f4e5154b61be4f9a'
