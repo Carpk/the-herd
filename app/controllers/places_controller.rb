9330aac84a55169e67bd1f755dfbc050b61e47c4
Promoveo <zappeln@gmail.com>
2014-03-22 19:29:28 -0500

    added color basic css

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def index
     place_id = Place.find_by_name(params[:name]).id
-    @chart = Traffic.traffic_report(place_id) 
+    @report = Traffic.traffic_report(place_id) 
   end
 end
