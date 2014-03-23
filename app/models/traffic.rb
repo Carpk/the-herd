c230ac6e389ea61de90abcd518f613fc727b2525
Promoveo <zappeln@gmail.com>
2014-03-23 14:34:56 -0500

    revert to working state

--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -3,14 +3,12 @@ class Traffic < ActiveRecord::Base
   belongs_to :place
 
   def self.traffic_report(store_id)
-    puts "STORE ID: #{store_id}"
     location_report = Traffic.where(place_id: store_id).all
-    puts "ALL FROM LOCATION: #{location_report}"
     chart = {}
     location_report.each do |t|
 
-      if t.time.strftime('%A') == Time.now.strftime('%A')
-        puts "PASS INTO HASH"
+      if (t.time - 5*60*60).strftime('%A') == Time.now.strftime('%A')
+        puts "VALIDATE: #{t.time} "
         chart.merge!({t.time => t.wait})
       end
     end
